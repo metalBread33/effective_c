ef1409ebffa90a4f50e333a650e29eea4314d8fb
metalBread33 <kfoughty80@gmail.com>
2024-05-04 22:43:51 -0400

    Add notes from ch1

---
+++ ch1.c
@@ -0,0 +1,7 @@
+//INLCUDE statements, used to use functions not in compiler
+#include <stdio.h> 
+#include <stdlib.h>
+int main (void) {   //main functions --> where program is starts
+    puts("Hello World"); //print text to screen
+    return EXIT_SUCCESS; //macro, lets user know everything worked
+}
