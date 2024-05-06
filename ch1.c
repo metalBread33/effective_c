3ac5ce553f6d5c8e9c49b64f98cced336e089ec3
metalBread33 <kfoughty80@gmail.com>
2024-05-06 10:52:41 -0400

    Add more notes

--- ch1.c
+++ ch1.c
@@ -3,5 +3,8 @@
 #include <stdlib.h>
 int main (void) {   //main functions --> where program is starts
     puts("Hello World"); //print text to screen
+
+    //Another way to print to terminal
+    printf("This is another way to print %s and numbers like: %d\n", "strings", 51);
     return EXIT_SUCCESS; //macro, lets user know everything worked
 }
